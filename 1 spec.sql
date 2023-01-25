9fef05bf5b83e3a9560a5206661cc926198be01c
sorahza <sorahza@gmail.com>
2023-01-24 19:59:09 -0500

    sample data added

--- 1 spec.sql
+++ 1 spec.sql
@@ -35,3 +35,53 @@
 
 */
 
+/*Sample Data
+LastName	FirstName	Class	stop	AM	PM	Friday
+Lincoln	Abraham	4-101	19th ave corner 45th st	A-JKL	P-JKL	F-JKL
+Jackson	Andrew	8-100	13th ave corner 50th st	A-ABC	P-ABC	F-ABC
+Johnson	Andrew	4-101	18th ave corner 48th st	A-JKL	P-JKL	F-JKL
+Obama	Barack	8-104	17th ave corner 39th st	A-GHI	P-GHI	F-GHI
+Harrison	Benjamin	4-102	15th ave corner 54th st	A-DEF	P-DEF	F-DEF
+Coolidge	Calvin	6-102	13th ave corner 45th st	A-ABC	P-ABC	F-ABC
+Arthur	Chester	4-102	13th ave corner 54th st	A-ABC	P-ABC	F-ABC
+Trump	Donald	4-104	18th ave corner 39th st	A-JKL	P-JKL	F-JKL
+Eisenhower	Dwight David	8-103	17th ave corner 45th st	A-GHI	P-GHI	F-GHI
+Pierce	Franklin	4-101	19th ave corner 45th st	A-JKL	P-JKL	F-JKL
+Roosevelt	Franklin Delano	5-103	15th ave corner 45th st	A-DEF	P-DEF	F-DEF
+Washington	George	8-100	15th ave corner 46th st	A-DEF	P-DEF	F-DEF
+Bush	George H. W.	4-104	14th ave corner 39th st	A-DEF	P-DEF	F-DEF
+Bush	George W.	8-104	16th ave corner 39th st	A-GHI	P-GHI	F-GHI
+Ford	Gerald R.	3-103	11th ave corner 39th st	A-ABC	P-ABC	F-ABC
+Cleveland	Grover	4-102	14th ave corner 54th st	A-DEF	P-DEF	F-DEF
+Cleveland	Grover	4-102	16th ave corner 54th st	A-GHI	P-GHI	F-GHI
+Truman	Harry S	8-103	16th ave corner 45th st	A-GHI	P-GHI	F-GHI
+Hoover	Herbert C.	4-103	14th ave corner 45th st	A-DEF	P-DEF	F-DEF
+Garfield	James	0-101	12th ave corner 54th st	A-ABC	P-ABC	F-ABC
+Madison	James	8-100	15th ave corner 46th st	A-DEF	P-DEF	F-DEF
+Monroe	James	8-100	16th ave corner 46th st	A-GHI	P-GHI	F-GHI
+Buchanan	James	4-101	18th ave corner 45th st	A-JKL	P-JKL	F-JKL
+Carter	James Earl	4-103	12th ave corner 39th st	A-ABC	P-ABC	F-ABC
+Polk	James K.	4-101	15th ave corner 49th st	A-DEF	P-DEF	F-DEF
+Biden	Joe	6-104	19th ave corner 39th st	A-JKL	P-JKL	F-JKL
+Tyler	John	4-100	14th ave corner 52nd st	A-DEF	P-DEF	F-DEF
+Adams	John	4-100	16th ave corner 46th st	A-GHI	P-GHI	F-GHI
+Kennedy	John Fitzgerald	2-103	18th ave corner 45th st	A-JKL	P-JKL	F-JKL
+Adams	John Quincy	4-100	17th ave corner 46th st	A-GHI	P-GHI	F-GHI
+Johnson	Lyndon Baines	6-103	19th ave corner 44th st	A-JKL	P-JKL	F-JKL
+van Buren	Martin	4-100	13th ave corner 50th st	A-ABC	P-ABC	F-ABC
+Fillmore	Millard	3-101	18th ave corner 45th st	A-JKL	P-JKL	F-JKL
+Nixon	Richard Milhous	5-103	20th ave corner 45th st	A-JKL	P-JKL	F-JKL
+Reagan	Ronald Wilson	8-103	13th ave corner 39th st	A-ABC	P-ABC	F-ABC
+Hayes	Rutherford	4-101	12th ave corner 54th st	A-ABC	P-ABC	F-ABC
+Roosevelt	Theodore	8-102	18th ave corner 54th st	A-JKL	P-JKL	F-JKL
+Jefferson	Thomas	8-100	17th ave corner 46th st	A-GHI	P-GHI	F-GHI
+Grant	Ulysses S.	8-101	19th ave corner 48th st	A-JKL	P-JKL	F-JKL
+Harding	Warren	2-102	12th ave corner 45th st	A-ABC	P-ABC	F-ABC
+McKinley	William	4-102	17th ave corner 54th st	A-GHI	P-GHI	F-GHI
+Taft	William	4-102	19th ave corner 54th st	A-JKL	P-JKL	F-JKL
+Harrison	William H.	7-100	13th ave corner 50th st	A-ABC	P-ABC	F-ABC
+Clinton	William Jefferson	8-104	15th ave corner 39th st	A-DEF	P-DEF	F-DEF
+Wilson	Woodrow	8-102	20th ave corner 54th st	A-JKL	P-JKL	F-JKL
+Taylor	Zachary	1-101	16th ave corner 50th st	A-GHI	P-GHI	F-GHI
+
+*/
